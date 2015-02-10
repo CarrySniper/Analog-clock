002575d50c9bcb3e95a52d5d8489609695f23341
cjq002 <692771080@qq.com>
2015-02-10 14:26:22 +0800

    Initial Commit

---
+++ 绘制模拟时钟/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  绘制模拟时钟
+//
+//  Created by 陈家庆 on 15-2-10.
+//  Copyright (c) 2015年 shikee_Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
